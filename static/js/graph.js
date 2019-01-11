07805b8f1648a9734b3c7907d5f64d3898586e97
Kirsten Allen <kmaaallen@gmail.com>
2019-01-11 16:30:25 +0000

    added stacked bar chart for gender distribution

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,23 +1,24 @@
 queue()
-    .defer(d3.csv,"data/Salaries.csv")
+    .defer(d3.csv, "data/Salaries.csv")
     .await(makeGraphs);
 
 function makeGraphs(error, salariesData) {
     var ndx = crossfilter(salariesData);
-    
-    salariesData.forEach(function(d){
+
+    salariesData.forEach(function(d) {
         d.salary = parseInt(d.salary);
     });
 
     show_gender_balance(ndx);
     show_discipline_selector(ndx);
     show_average_salary(ndx);
+    show_rank_distribution(ndx);
 
     dc.renderAll();
 }
 
-function show_gender_balance(ndx){
-    var dim=ndx.dimension(dc.pluck('sex'));
+function show_gender_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
     dc.barChart("#gender-balance")
         .width(400)
@@ -33,46 +34,50 @@ function show_gender_balance(ndx){
         .yAxis().ticks(20);
 }
 
-function show_discipline_selector(ndx){
+function show_discipline_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('discipline'));
     var group = dim.group();
-    
+
     dc.selectMenu('#discipline-selector')
-    .dimension(dim)
-    .group(group);
+        .dimension(dim)
+        .group(group);
 }
 
-function show_average_salary(ndx){
-    var dim =ndx.dimension(dc.pluck('sex'));
-    function add_item(p,v){
-        p.count ++;
+function show_average_salary(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+
+    function add_item(p, v) {
+        p.count++;
         p.total += v.salary;
-        p.average = p.total/p.count;
+        p.average = p.total / p.count;
         return p;
     }
-    function remove_item(p,v){
-        p.count --;
-        if (p.count == 0){
+
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
             p.total = 0;
             p.average = 0;
-        }else{
-            p.total -=v.salary;
-            p.average = p.total/p.count;
         }
-         return p;
+        else {
+            p.total -= v.salary;
+            p.average = p.total / p.count;
+        }
+        return p;
     }
-    function initialise(){
-        return {count:0, total:0, average:0};
-    }
-var averageSalaryByGender = dim.group().reduce(add_item, remove_item, initialise);
 
-dc.barChart("#average-salary")
+    function initialise() {
+        return { count: 0, total: 0, average: 0 };
+    }
+    var averageSalaryByGender = dim.group().reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#average-salary")
         .width(400)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(averageSalaryByGender)
-        .valueAccessor(function(d){
+        .valueAccessor(function(d) {
             return d.value.average.toFixed(2);
         })
         .transitionDuration(500)
@@ -81,5 +86,55 @@ dc.barChart("#average-salary")
         .elasticY(true)
         .xAxisLabel("Gender")
         .yAxis().ticks(4);
-    
 }
+
+function show_rank_distribution(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+
+    function rankByGender(dimension, rank) {
+        return dimension.group().reduce(
+            function(p, v) {
+                p.total++;
+                if (v.rank == rank) {
+                    p.match++;
+                }
+                return p;
+            },
+            function(p, v) {
+                p.total--;
+                if (v.rank == rank) {
+                    p.match--;
+                }
+                return p;
+            },
+
+            function() {
+                return { total: 0, match: 0 };
+            }
+        );
+    }
+    var profByGender = rankByGender(dim, "Prof");
+    var asstProfByGender = rankByGender(dim, "AsstProf");
+    var assocProfByGender = rankByGender(dim, "AssocProf");
+    
+    dc.barChart("#rank-by-gender")
+        .width(400)
+        .height(300)
+        .dimension(dim)
+        .group(profByGender, "Prof")
+        .stack(asstProfByGender, "Asst Prof")
+        .stack(assocProfByGender, "Assoc Prof")
+        .valueAccessor(function(d) {
+            if(d.value.total > 0) {
+                return (d.value.match / d.value.total) * 100;
+            } else {
+                return 0;
+            }
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
+        .margins({top: 10, right: 100, bottom: 30, left: 30});
+}
+
+    
