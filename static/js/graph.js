850ece87f460fb908f892c17f223da1a5ba29efc
Kirsten Allen <kmaaallen@gmail.com>
2019-01-11 17:21:30 +0000

    displaying number percentage of professors for men and women

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,14 +7,66 @@ function makeGraphs(error, salariesData) {
 
     salariesData.forEach(function(d) {
         d.salary = parseInt(d.salary);
-    });
-
-    show_gender_balance(ndx);
+    })
+    
     show_discipline_selector(ndx);
+
+    show_percent_that_are_professors(ndx, "Female", "#percent-of-women-professors");
+    show_percent_that_are_professors(ndx, "Male", "#percent-of-men-professors");
+    show_gender_balance(ndx);
+
     show_average_salary(ndx);
     show_rank_distribution(ndx);
 
     dc.renderAll();
+}
+
+function show_discipline_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('discipline'));
+    var group = dim.group();
+
+    dc.selectMenu('#discipline-selector')
+        .dimension(dim)
+        .group(group);
+}
+
+function show_percent_that_are_professors(ndx, gender, element) {
+    var percentageThatAreProf = ndx.groupAll().reduce(
+        function(p, v) {
+            if (v.sex == gender) {
+                p.count++;
+                if (v.rank == "Prof") {
+                    p.areProf++;
+                }
+            }
+            return p;
+        },
+
+        function(p, v) {
+            if (v.sex == gender) {
+                p.count--;
+                if (v.rank == "Prof") {
+                    p.areProf--;
+                }
+            }
+            return p;
+        },
+
+        function() {
+            return { count: 0, areProf: 0 };
+        }
+    );
+    dc.numberDisplay(element)
+        .formatNumber(d3.format(".2%"))
+        .valueAccessor(function(d) {
+            if (d.count == 0) {
+                return 0;
+            }
+            else {
+                return (d.areProf / d.count);
+            }
+        })
+        .group(percentageThatAreProf);
 }
 
 function show_gender_balance(ndx) {
@@ -34,14 +86,6 @@ function show_gender_balance(ndx) {
         .yAxis().ticks(20);
 }
 
-function show_discipline_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('discipline'));
-    var group = dim.group();
-
-    dc.selectMenu('#discipline-selector')
-        .dimension(dim)
-        .group(group);
-}
 
 function show_average_salary(ndx) {
     var dim = ndx.dimension(dc.pluck('sex'));
@@ -116,7 +160,7 @@ function show_rank_distribution(ndx) {
     var profByGender = rankByGender(dim, "Prof");
     var asstProfByGender = rankByGender(dim, "AsstProf");
     var assocProfByGender = rankByGender(dim, "AssocProf");
-    
+
     dc.barChart("#rank-by-gender")
         .width(400)
         .height(300)
@@ -125,16 +169,15 @@ function show_rank_distribution(ndx) {
         .stack(asstProfByGender, "Asst Prof")
         .stack(assocProfByGender, "Assoc Prof")
         .valueAccessor(function(d) {
-            if(d.value.total > 0) {
+            if (d.value.total > 0) {
                 return (d.value.match / d.value.total) * 100;
-            } else {
+            }
+            else {
                 return 0;
             }
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 });
 }
-
-    
